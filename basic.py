40775e04dbf70657c084eda8d99327fa3becf4f2
Syed Amaan Mehmoodi <146960899+amaan4n@users.noreply.github.com>
2023-10-28 14:26:03 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,26 @@
+print("hello World 1")
+#it is single comment
+""" 
+this is multi line comment
+hccfubi
+hgchgbjn
+"""
+#variable
+number = 26
+print(number)
+
+myNameIs = "amaan"
+word = 'n'
+
+print(myNameIs)
+print(word)
+
+#datatype
+
+num = 99 #integer
+alphabet = "sam" #string
+num1 = 99.0000 #float
+bool = True #boolean
+type(bool)
+print(type(bool))
+
